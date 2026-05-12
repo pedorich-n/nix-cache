dfbe988de4d34af7a801ad9c036af83f97f3fe6b
Nikita Pedorich <pedorich.n@gmail.com>
2026-05-13 08:59:53 +0900

    Add a worker to handle basic routes & redirects

--- docs/cloudflare.md
+++ docs/cloudflare.md
@@ -4,7 +4,9 @@ For TF code in this repository to work it needs a Cloudflare API Token with the 
 
 - Account -> Workers R2 Storage -> Edit (to create a bucket)
 - Account -> Account Rulesets -> Read (to read existing rulesets)
+- Account -> Workers Scripts -> Edit (to create a redirects worker)
 - User -> API Tokens -> Edit (to create bucket access key)
 - Zone -> Transform Rules -> Edit (to create a redirection from `/` -> `/index.html` for a public bucket url)
+- Zone -> Workers Routes -> Edit (to create a worker route)
 - Zone -> Zone -> Read (to reference the zone in Terraform)
 - Zone -> DNS -> Edit (to create DNS records for the niks3 server)
---
+++ terraform/workers.tf
@@ -0,0 +1,45 @@
+resource "cloudflare_worker" "niks3_redirects" {
+  account_id = local.cf_account_id
+  name       = "niks3-redirect-worker"
+  observability = {
+    enabled = true
+  }
+}
+
+resource "cloudflare_worker_version" "niks3_redirects_version" {
+  account_id         = local.cf_account_id
+  worker_id          = cloudflare_worker.niks3_redirects.id
+  compatibility_date = "2026-05-12"
+  main_module        = "worker.js"
+  modules = [
+    {
+      name         = "worker.js"
+      content_type = "application/javascript+module"
+      content_file = local.workers_script_path
+    }
+  ]
+
+  bindings = [
+    {
+      name = "R2_BUCKET_PUBLIC_URL"
+      text = local.r2_bucket_public_url
+      type = "plain_text"
+    }
+  ]
+}
+
+resource "cloudflare_workers_deployment" "niks3_redirects_deployment" {
+  account_id  = local.cf_account_id
+  script_name = cloudflare_worker.niks3_redirects.name
+  strategy    = "percentage"
+  versions = [{
+    percentage = 100
+    version_id = cloudflare_worker_version.niks3_redirects_version.id
+  }]
+}
+
+resource "cloudflare_workers_route" "niks3_redirects" {
+  zone_id = local.cf_zone_id
+  pattern = "${local.niks3_server_domain}/*"
+  script  = cloudflare_worker.niks3_redirects.name
+}
