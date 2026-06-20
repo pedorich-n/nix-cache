0e2a99976eef80e29abd58a52bb45897b9f7b76c
Nikita Pedorich <pedorich.n@gmail.com>
2026-06-20 12:48:50 +0900

    Switch back to upstream niks3

--- flake.nix
+++ flake.nix
@@ -5,11 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
-
-    niks3 = {
-      url = "github:Mic92/niks3";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
